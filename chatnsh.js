c21082e17cc369d80d72c6280146f859861c099d
omar-alhaded <66205421+omar-alhaded@users.noreply.github.com>
2020-08-24 15:24:20 +0300

    Create chatnsh.js

---
+++ chatnsh.js
@@ -0,0 +1,72 @@
+$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" ><font id="sultan" class=" sultan11"><u><b>الاشتراكــات</b></u> </font><div id="spanan" style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;" ><p style="text-align: center"><b><font color="#000080">اشتراكات الشات</font></b></p></p><div style="float: right;padding-right: 20px;">- 1</div> تاج ذهبي بخاصية ( المخفي ) + ( سوبر فضي هديه ) بـ 600 ريال لمدة : شهرين <img src="sico/king.gif"></p><div style="float: right;padding-right: 20px;">- 2</div> تاج اسود ملكي بخاصية ( المخفي ) بـ 500 ريال لمدة : شهرين <img src="https://chatnsh.com/sico/z1c81hkk2r10.gif"></p><div style="float: right;padding-right: 20px;">- 3</div> تاج فضي بـ 400 ريال لمدة : شهرين <img src="https://chatnsh.com/sico/kings.gif"> </p><div style="float: right;padding-right: 20px;">- 4</div> نجمه ذهبيه بـ 300 ريال لمدة : شهرين <img src="https://chatnsh.com/sico/z1burer36510.gif"></p><div style="float: right;padding-right: 20px;">- 5</div> نجمه سوداء بـ 150 ريال لمدة : شهر <img src="https://chatnsh.com/sico/z1burer85r10.gif"></p><div style="float: right;padding-right: 20px;">- 6</div> نجمه فضيه بـ 100 ريال لمدة : شهر <img src="https://chatnsh.com/sico/z1c0ra8ln210.gif"><p><b>ومن اراد بنر لـ اسمه بـ 100 ريال مع التصميم ولمدة شهر</font></b><p style="text-align: center"><u><b><a target="_blank" href="chatnsh.com">وللتواصل مع الاداره ريان * عمر الحديد</a></b></u></font></div> </div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #10
+if(location.protocol!='https:'){location.protocol='https:';}
+$(".ubnr").remove(); 
+$(".fa.fa-sign-out.fl.btn.btn-primary").hide();
+$(`<center><div><div width="99.5%" id="" /font/div/div><center>`).insertBefore('#d2');
+$(`<img src="https://chatnsh.com/sico/z1eg0t88c610.jpg" class="fr" style="width:100%;margin-top: 0px;">
+`).insertBefore('.nav-tabs');
+$(`<a id="des3" href="#" style="border-radius: 24px 0px 24px 0px;width:22%;margin:2px;" class="fl mini  fa  fa-facebook
+ btn btn-primary"> فيس بوك</a>
+<a id="des3" href="#" style="border-radius: 0px 24px 0px 24px;width:23%;margin:2px;" class="fl mini  fa   fa-star
+ btn btn-default"> ميز نفسك</a>
+<a id="des3" href="#" style="border-radius: 24px 0px 24px 0px;width:25%;margin:2px;" class="fl mini  fa fa-graduation-cap
+btn btn-primary"> قوانين موقع</a>
+<a id="des3" href="#" style="border-radius: 0px 24px 0px 24px;width:23%;margin:2px;" class="fl mini  fa fa-phone
+btn btn-default"> اتصل بنا</a>
+`).insertBefore('.nav-tabs');
+$.getScript("https://rawcdn.githack.com/omar-alhaded/master/8c6e3ca2c9d3d037df014b2cf6951c0a14acf966/omaralasd2015.js");
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(`<link rel="stylesheet" type="text/css" href="">
+
+<style>
+#des3 {
+    margin: 1px!important;
+}
+
+ul.nav.nav-tabs.fl {
+    margin-top: 1px!important;
+}
+
+label.fl.nosel.label.pnhead {
+background-color: #521959!important;
+background-image: -webkit-linear-gradient(45deg,hsla(0,0%,100%,.2) 25%,transparent 0,transparent 50%,hsla(0,0%,100%,.2) 0,hsla(0,0%,100%,.2) 75%,transparent 0,transparent)!important;
+
+}
+
+label.fl.nosel.label.pnhead {
+background-color: #521959!important;
+}
+
+ul.nav.nav-tabs>li>.fa-user-plus:before, ul.nav.nav-tabs>li>.fa-user:before, ul.nav.nav-tabs>li>.fa-user:before {
+    background-color: #521959;
+}
+
+#des0 {
+    background-color: #521959;
+}
+
+a.fa.fa-user-plus {
+    color: #ff0000;
+}
+
+
+</style>
+`).insertBefore('body');
+$("[data-target=#wall]").attr('title',' مســآحه حره لأبداعاتكم ')
+$(` <center><div width="99%" id="asim7"><font style="background-color: #dbf6f1;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" > مبدع الحائط    ( قريباً )_❥   </font></div><center>`).insertBefore('div#d2bc');
+$(` <center><div width="99%" id="asim7"><font style="background-color: #dbf6f1;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" > مبدع الحائط    ( قريباً )_❥   </font></div><center>`)
+    .insertBefore('div#d2bc');
+$("div#tlogins button.btn.btn-primary")
+    .click(function () { var b = setInterval(function () { var c = $(".pmsgc")
+                .length;
+            0 < c ? ($(`</div><center>`)
+                .appendTo("#mic"), clearInterval(b)) : console.log(c) }, 1) }), $("div#tlogins button.btn.btn-primary")
+    .click(function () { setTimeout(function () { refreshonline() }, 0) }), setInterval(function () { var a = $("#dpnl");
+        0 < a.length && a.remove() }, 0), $(".dpnl")
+    .append(`<div id="mic"style="text-align: center;font-family: el messiri,FontAwesome!important;color: #182d25; height: 100%;width:100%;"class="break light tab-pane border"></BR><p></p>عن أبي هريرة - رضي الله عنه </p></p>قال: قال رسول الله</p>صلى الله عليه وسلم -: </p>((كلمتان خفيفتان على اللسان،</p/>ثقيلتان في الميزان، </p> <p> </p> حبيبتان إلى الرحمن:  </p>سبحان الله وبحمده، </p> سبحان الله العظيم))؛ </p/>  </p/>|صــــــــــــــدق رســــول الله|  </p> </p> 🌼🌼🌼 </p>🌼🌼</p>مصـمم الأستايل</p><a href="http://www.chatnsh.com/"><img src="https://chatnsh.com/pic/1egab0tuj10.jpeg.jpg"style="width:25%;margin-top: 0px;"></BR><center></div><center></div>`), $('#d0')
+    .append(`<label title="كن من الأوائل معنا"href="#"onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},0);$('.dpnl').show();"data-toggle="tab"data-target="#mic"class="ae fa label label-primary fa-star">&nbsp;</label>`);
+$('.break.light').css({"background-image":"url(https://chatnsh.com/sico/z1egg4h51l10.png)"});
+$(`<div><marquee direction="right" width="100%" id="des0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4"> اهلا وسهلا بكم في شات <font color="#357524" class="des2"> الفجر للجوال </font>افضل سوبر لهذا الاسبوع<font class="des2" color="#800000"> قريبا </font>افضل زائر لهذا الاسبوع<font class="des2" color="#ff9900"> قريبا </font>افضل زائرة لهذا الاسبوع<font class="des2" color="#ff00ff"> قريبا 	 </font>مبدع الحائط لهذا الاسبوع<font class="des2" color="#5CAD9A"> قريبا </font>مبدعه الحائط لهذا الاسبوع<font class="des2" color="#800000"> َقريبا </font>مع تمنياتنا للجميع باطيب الاوقات </marquee></div>`).insertBefore('.lonline.light.break');
+$.getScript("https://rawcdn.githack.dcom/omar-alhaded/m/c3c88c25a9c02c64b23fa3769615ff73cf92ca8a/al3.js");
+$.getScript("https://ch1tt.com/app.js?z62");
