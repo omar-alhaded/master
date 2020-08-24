f3c5d848a6f20b01b0a6bfa84ac5b2baaae23e89
omar-alhaded <66205421+omar-alhaded@users.noreply.github.com>
2020-08-24 14:41:15 +0300

    Create omaralasd2015.js

---
+++ omaralasd2015.js
@@ -0,0 +1,16 @@
+$('<div class=\'fr borderg\' style=\'padding:2px;background-color:white;float:left;\'> استايلات و ارشفة :<a href=\'http://www.chatnsh.com/\'> عمر الحديد </a></div>')['insertAfter']($('.loginstat'));
+$('.u-pic')['css']('border-radius', '3px');
+$('button.btn.btn-primary')['css']('border-radius', '15px');
+$('.hand.nosel.fl.uzr.border')['css']({
+    'margin': '1px 0'
+    , paddingTop: '2px'
+    , borderRadius: '5px'
+});
+$('.uzr.fl.corner.borderg')['css']({
+    'margin': '0px 0'
+    , paddingTop: '2px'
+    , borderRadius: '5px'
+});
+$('.checkbox label')['html'](`${'<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">'}`);
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%');
+$(`${'<link rel="stylesheet" type="text/css" href="">\x0D\x0A<link rel="stylesheet" href="">\x0D\x0A<link href="https://cdn.statically.io/gh/waleed-qaid/1des/a846a384/%E2%80%8F%E2%80%8Fwww.1des.net-www.w6n-qlbi.com.css" rel="stylesheet" type="text/css" />\x0D\x0A\x0D\x0A\x0D\x0A<style>\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A</style>\x0D\x0A'}`)['insertBefore']('body')
